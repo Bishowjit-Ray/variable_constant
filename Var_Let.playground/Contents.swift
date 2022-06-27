b470d6d707caeb949c5fdade889304f8f106eb83
Bishowjit-Ray <82141518+Bishowjit-Ray@users.noreply.github.com>
2022-06-27 16:22:33 +0600

    Add files via upload

---
+++ Var_Let.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+//Variablew
+var str = "Hello, playground"
+
+var str2 = 43
+print(str)
+print(str2)
+str = "Bangladesh"
+print(str)
+
+//Constant
+let sh = "Swift lover"
+print(sh)
+
+
+print("My country name is \(str)")
+
+// check Empty string
+let A = ""
+
+if A.isEmpty {
+    print("A is empty")
+}else{
+    print("no,A is  not empty")
+}
+
+//Compare String
+let B = "AA"
+let C = "BB"
+
+if B==C {
+    print("Yes, Both of equal")
+}
+else{
+    print("Not same")
+}
+
+
+
